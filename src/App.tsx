d8cc4e946c995b904f9428999b56e46bed9d86ff
Jonathan Sibley <jonsibley@gmail.com>
2019-12-24 19:53:32 +0100

    log in screen laid out

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,82 @@
 import "antd/dist/antd.css";
 
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
 import { ApolloProvider } from "@apollo/react-hooks";
-import { Button } from "antd";
+import { LogIn } from "./LogIn";
 import React from "react";
 import { client } from "./apollo-client";
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <Button type="primary">Button</Button>
+      <div
+        className="App"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "32px 0px"
+        }}
+      >
+        <Router>
+          <div style={{ width: "800px" }}>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/log-in">Log In</Link>
+                </li>
+                <li>
+                  <Link to="/log-out">Log Out</Link>
+                </li>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/log-out">
+                <LogOut />
+              </Route>
+              <Route path="/log-in">
+                <LogIn />
+              </Route>
+              <Route path="/forgot-password">
+                <ForgotPassword />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
     </ApolloProvider>
   );
+}
+
+function Home() {
+  return <h2>Me</h2>;
+}
+
+function LogOut() {
+  return <h2>Log out</h2>;
+}
+
+function ForgotPassword() {
+  return <h2>ForgotPassword</h2>;
+}
+
+function Register() {
+  return <h2>Register</h2>;
 }
 
 export default App;
---
+++ src/LogIn.tsx
@@ -0,0 +1,65 @@
+import { Button, Checkbox, Form, Icon, Input } from "antd";
+import React, { FormEvent } from "react";
+
+import { FormComponentProps } from "antd/lib/form/Form";
+
+type Props = {};
+
+class NormalLoginForm extends React.Component<Props & FormComponentProps> {
+  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "Please input your username!" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input your Password!" }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("remember", {
+            valuePropName: "checked",
+            initialValue: true
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="/forgot-password">
+            Forgot password
+          </a>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Log in
+          </Button>
+          Or <a href="/register">register now!</a>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+export const LogIn = Form.create({ name: "normal_login" })(NormalLoginForm);
