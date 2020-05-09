eb599e2a17d5dded35c91ef03b6d5ef634a16d64
Jonathan Sibley <jonsibley@gmail.com>
2020-05-09 17:30:57 +0200

    integrate bugsnag
    Former-commit-id: 1cf808fdced6de1baa0b7d31b01ef3ed8b37d17d

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,24 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 import App from "./App";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+Bugsnag.start({
+  apiKey: "cab2c44ac75b0030bbdd178046a5b1da",
+  plugins: [new BugsnagPluginReact(React)],
+});
+
+const ErrorBoundary = Bugsnag.getPlugin("react");
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
